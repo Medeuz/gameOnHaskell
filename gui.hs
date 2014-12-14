e419fc62eed3726112099fa9afcc546cb2c1e168
Hequs <alexpetr-mail@yandex.ru>
2014-12-14 21:11:03 +0300

    Навел порядок. Написал умные вещи.

--- gui.hs
+++ gui.hs
@@ -1,68 +1,37 @@
-﻿import Graphics.UI.WX
+import Graphics.UI.WX
+import Data.Array.IArray
+import Data.List.Split
+
+btnLabel :: Int -> String
+btnLabel x = if (x == 16) then "" else (show x)
+
+btnList :: Window a -> Array Int Int -> IO ([Button ()])
+btnList wnd arr = sequence $ map (\x -> button wnd [text := (btnLabel x)]) (elems arr)
+
+btnSet :: (Form f, Widget w) => f -> [w] -> IO ()
+btnSet wnd btns = set wnd [layout := column 3 $ map (\x -> margin 3 $ row 3 (map widget x)) (chunksOf 4 btns)]
+
+redrawGui :: Array Int Int -> IO ()
+redrawGui a = undefined
 
 main :: IO ()
 main = start gui
 
 gui :: IO ()
 gui = do
-  -- форма, на которой будут лежать все наши контролы
+  -- создаем окно
   wnd <- frame [ text := "Пятнашки", size := sz 300 300, bgcolor := blue ]
   
-  -- несколько кнопок фиксированного размера
-  --которые отображают наше поле
-  button1 <- button wnd [ text := "1"] 
-  button2 <- button wnd [ text := "2", clientSize := sz 25 25 ]
-  button3 <- button wnd [ text := "3", clientSize := sz 25 25 ]
-  button4 <- button wnd [ text := "4", clientSize := sz 25 25 ]
-  button5 <- button wnd [ text := "5", clientSize := sz 25 25 ]
-  button6 <- button wnd [ text := "6", clientSize := sz 25 25 ]
-  button7 <- button wnd [ text := "7", clientSize := sz 25 25 ]
-  button8 <- button wnd [ text := "8", clientSize := sz 25 25 ]
-  button9 <- button wnd [ text := "9", clientSize := sz 25 25 ]
-  button10 <- button wnd [ text := "10", clientSize := sz 25 25 ]
-  button11 <- button wnd [ text := "11", clientSize := sz 25 25 ]
-  button12 <- button wnd [ text := "12", clientSize := sz 25 25 ]
-  button13 <- button wnd [ text := "13", clientSize := sz 25 25 ]
-  button14 <- button wnd [ text := "14", clientSize := sz 25 25 ]
-  button15 <- button wnd [ text := "15", clientSize := sz 25 25 ]
-  button16 <- button wnd [ text := "", clientSize := sz 25 25 ]
+  -- создаем список кнопок по массиву
+  let btnsArray = array (1,16) [(i,i) | i <- [1..16]]
+  btns <- btnList wnd btnsArray
+  
+  -- прикрепляем список кнопок к окну
+  btnSet wnd btns
+  
  --задаем действия по клику на кнопку
   --set button1 [ on (charKey 'S') := 
 		 --set button1 [ text := if (get button5 text) /= "" then (get button5 text) else ""] 
 		 --set button5 [ text := if btnText /= "" then "" else btnText]
 		 
-  -- прикрепл¤ем контролы к окну
-  set wnd [
-    layout := 
-      column 3 [
-        {- 1  -}
-        margin 3 $ row 3 [
-		  widget button1,
-		  widget button2,
-		  widget button3,
-		  widget button4
-        ],      
-        {- 2  -}
-        margin 3 $ row 3 [
-          widget button5,
-		  widget button6,
-		  widget button7,
-		  widget button8
-        ],
-		{- 3 -}
-        margin 3 $ row 3 [
-          widget button9,
-		  widget button10,
-		  widget button11,
-		  widget button12
-        ],
-		{- 4 -}
-        margin 3 $ row 3 [
-          widget button13,
-		  widget button14,
-		  widget button15,
-		  widget button16
-        ]
-      ]
-    ]
   return ()
